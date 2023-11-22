a845dede733d10ffc022dea9662641e418a43de0
accurpress <amir.h.ebrahimnezhad@gmail.com>
2023-11-21 17:30:37 -0800

    Update Ep4_Streaming

---
+++ src/Ep4_Streaming/image_publisher.py
@@ -0,0 +1,54 @@
+#!/home/username/parrot/bin/python
+
+import rospy
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from sensor_msgs.msg import Image
+import rospkg
+import yaml
+import cv2
+import os
+
+rospack = rospkg.RosPack()
+config_path = rospack.get_path('deep_ros') + '/config/param.yaml'
+
+print(config_path)
+
+with open(config_path, 'r') as stream:
+    config = yaml.safe_load(stream)
+
+IMAGE_WIDTH = config['image_width']
+IMAGE_HEIGHT = config['image_height']
+ROS_RATE = config['ros_rate']
+CAMERA_TOPIC = config['camera_topic']
+
+
+if __name__ == "__main__":
+    rospy.init_node('image_pub_node')
+
+    image_pub = rospy.Publisher(CAMERA_TOPIC, Image, queue_size=10)
+   
+
+    rospack = rospkg.RosPack()
+    workspace_dir = rospack.get_path('deep_ros')
+
+    rate=rospy.Rate(ROS_RATE)
+
+    bridge = CvBridge()
+
+    frame_path = workspace_dir + "/repo/frames/"
+
+    frame_list = os.listdir(frame_path)
+    frame_list.sort()
+    i = 0
+
+    while not rospy.is_shutdown():
+
+        img = cv2.imread(os.path.join(frame_path, "img_sphinx.jpg"))
+        image_pub.publish(bridge.cv2_to_imgmsg(img, "bgr8"))
+        print(img.shape)
+
+        rate.sleep()
+
+        
+    rospy.loginfo("Node has been stopped")
--- src/Ep4_Streaming/sp_olympe.py
+++ src/Ep4_Streaming/sp_olympe.py
@@ -5,6 +5,19 @@ import sys
 import rospy
 from geometry_msgs.msg import Twist
 import time
+import csv
+import math
+import os
+import queue
+import shlex
+import subprocess
+import tempfile
+import threading
+
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from sensor_msgs.msg import Image
+
 
 import olympe
 from olympe.messages.ardrone3.Piloting import TakeOff, Landing, PCMD
@@ -18,19 +31,82 @@ class OlympeBridge():
 
     def __init__(self):
         rospy.init_node('olympe_bridge_node', anonymous=False)
-        rospy.Subscriber('/parrot/cmd_vel', Twist, self.cmd_vel_callback)
-
-        self.rate = rospy.Rate(10)
 
         self.drone = olympe.Drone(DRONE_IP)
-        self.drone.connect()
+        self.tempd = tempfile.mkdtemp(prefix="olympe_streaming_test_")
+        print("Olympe streaming example output dir: {}".format(self.tempd))
+        
+        self.h264_frame_stats = []
+        self.h264_stats_file = open(os.path.join(self.tempd, "h264_stats.csv"), "w+")
+        self.h264_stats_writer = csv.DictWriter(self.h264_stats_file, ["fps", "bitrate"])
+        self.h264_stats_writer.writeheader()
+        self.frame_queue = queue.Queue()
+        self.processing_thread = threading.Thread(target=self.yuv_frame_processing)
+        self.renderer = None
 
-        assert self.drone(TakeOff()).wait().success()
+
+        rospy.Subscriber('/parrot/cmd_vel', Twist, self.cmd_vel_callback)
+        self.image_pub = rospy.Publisher("/parrot/image_raw",Image, queue_size=10)
+
+
+        self.bridge = CvBridge()
+        self.cv2_cvt_color_flag = {
+                    olympe.VDEF_I420: cv2.COLOR_YUV2BGR_I420,
+                    olympe.VDEF_NV12: cv2.COLOR_YUV2BGR_NV12,
+                }
+        
+        self.rate = rospy.Rate(10)
 
         self.cmd_vel = Twist()
         self.control = False
 
-        # rospy.spin()
+    def start(self):
+        # Connect the the drone
+        assert self.drone.connect(retry=3)
+
+        if DRONE_RTSP_PORT is not None:
+            self.drone.streaming.server_addr = f"{DRONE_IP}:{DRONE_RTSP_PORT}"
+
+        assert self.drone(TakeOff()).wait().success()
+
+        # Setup your callback functions to do some live video processing
+        self.drone.streaming.set_callbacks(
+            raw_cb=self.yuv_frame_cb,
+        )
+        # Start video streaming
+        self.drone.streaming.start()
+        # self.renderer = PdrawRenderer(pdraw=self.drone.streaming)
+        self.running = True
+        self.processing_thread.start()
+    
+    def yuv_frame_cb(self, yuv_frame):
+        """
+        This function will be called by Olympe for each decoded YUV frame.
+
+            :type yuv_frame: olympe.VideoFrame
+        """
+        yuv_frame.ref()
+        self.frame_queue.put_nowait(yuv_frame)
+
+    def yuv_frame_processing(self):
+        while self.running:
+            try:
+                yuv_frame = self.frame_queue.get(timeout=0.1)
+                x = yuv_frame.as_ndarray()
+                [yuv_frame.format()]
+                cv2frame = cv2.cvtColor(x, self.cv2_cvt_color_flag[yuv_frame.format()]) 
+                cv2.imwrite("/home/accurpress/catkin_ws/src/deep_ros/repo/frames/img_sphinx.jpg", cv2frame)
+                self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv2frame, "bgr8"))
+                print(cv2frame.shape)
+
+                
+
+            except queue.Empty:
+                continue
+            # You should process your frames here and release (unref) them when you're done.
+            # Don't hold a reference on your frames for too long to avoid memory leaks and/or memory
+            # pool exhaustion.
+            yuv_frame.unref()
 
     def cmd_vel_callback(self, msg):
         self.cmd_vel.linear.x, self.cmd_vel.linear.y, self.cmd_vel.linear.z = [msg.linear.x, msg.linear.y, msg.linear.z]
@@ -40,8 +116,8 @@ class OlympeBridge():
         self.drone(
             PCMD(
                 1,
-                int(-self.cmd_vel.linear.y),
                 int(self.cmd_vel.linear.x),
+                int(self.cmd_vel.linear.y),
                 int(-self.cmd_vel.angular.z),
                 int(self.cmd_vel.linear.z),
                 timestampAndSeqNum=0,
@@ -59,18 +135,20 @@ class OlympeBridge():
 
 
 def main(args):
-    oleympe_bridge = OlympeBridge()
+    olympe_bridge = OlympeBridge()
+    olympe_bridge.start()
 
     while not rospy.is_shutdown():
 
-        oleympe_bridge.run()
-        oleympe_bridge.rate.sleep()
+        olympe_bridge.run()
+        olympe_bridge.rate.sleep()
 
 
-    assert oleympe_bridge.drone(Landing()).wait().success()
+    olympe_bridge.drone(Landing())
     print("Shutting down")
 
 
 if __name__ == "__main__":
     main(sys.argv)
 
+    
