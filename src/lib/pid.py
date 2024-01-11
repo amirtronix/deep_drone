9b2286167f3fefebcdb70c4c53f2f89bc9223ad4
accurpress <amir.h.ebrahimnezhad@gmail.com>
2024-01-10 20:42:59 -0800

    Update pid.py

--- src/lib/pid.py
+++ src/lib/pid.py
@@ -2,21 +2,28 @@
 
 from collections import namedtuple
 
-Gains = namedtuple('Gains', 'x, y, z')
-
+class Gains:
+    def __init__(self):
+        self.kp = 0
+        self.ki = 0
+        self.kd = 0
 
 
 
 class PidController:
     def __init__(self, _gains, _sample_time, _anti_windup=None):
         
-        self.gains = Gains(0, 0, 0)
+        self.gains = Gains()
         self._setGains(_gains)
         self._ts = _sample_time
+        self._sum = 0
+        self._prev_error = 0
 
 
     def _setGains(self, _gains_list):
-        self.gains = Gains(_gains_list[0], _gains_list[1], _gains_list[2])
+        self.gains.kp = _gains_list[0]
+        self.gains.ki = _gains_list[1]
+        self.gains.kd = _gains_list[2]
 
     def _antiWindup(self):
         pass
@@ -25,15 +32,20 @@ class PidController:
         pass
 
     def _integral(self, error):
-        return 0
+        self._sum = self._sum + error*self._ts
+        return self._sum
 
     def _derivateive(self, error):
-        return 0
+        derivative = (error*self._prev_error)/self._ts
+        self._prev_error = error
+        return derivative
     
     def compute(self, error):
-        _contorlSignal = self.gains[0]*error + \
-            self.gains[1]*self._integral(error) + \
-            self.gains[1]*self._derivateive(error)
+        _contorlSignal = self.gains.kp*error + \
+            self.gains.ki*self._integral(error) + \
+            self.gains.kd*self._derivateive(error)
+        
+        return _contorlSignal
 
 
 controller = PidController()
