bd931a8c7e77c0e39a0f0895be16c1c27563a94c
accurpress <amir.h.ebrahimnezhad@gmail.com>
2024-01-18 18:14:23 -0800

    Edit folders

--- src/lib/pulse.py
+++ src/lib/pulse.py
@@ -1,8 +1,23 @@
 #!/home/username/parrot/bin/python
 
 class PulseGenerator:
-    def __init__(self, _gains, _sample_time, _anti_windup=None):
+    def __init__(self, pulse_width, amplitude, offset = 0):
         
-        pass
+        self._pulse_width = pulse_width
+        self._amplitude = amplitude
+        self._offset = offset
 
 
+    def _even_odd(self, num):
+        
+        if(int(num)%2 == 0):
+            return 1.0
+        
+        else:
+            return -1.0
+    
+
+    def generate(self, time):
+        
+        signal = self._even_odd(time/self._pulse_width)*self._amplitude + self._offset
+        return signal
