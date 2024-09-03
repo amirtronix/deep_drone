423f5472bba9547a691a0f17bfd67551d71e9374
amirtronics <amir.h.ebrahimnezhad@gmail.com>
2024-09-02 21:48:58 -0700

    pr_olympe: add status

--- src/parrot_ros/src/pr_olympe.py
+++ src/parrot_ros/src/pr_olympe.py
@@ -1,72 +1,83 @@
 #!/home/legatus/parrot/bin/python
 
+import sys
 import rospy
-import olympe
-import os
-from olympe.messages.ardrone3.Piloting import TakeOff, Landing, PCMD
-
+from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from loguru import logger
+import os
+import olympe
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing, PCMD
 
 DRONE_IP = os.environ.get("DRONE_IP", "10.202.0.1")
 
 class OlympeBridge():
     def __init__(self):
-        rospy.init_node("olympe_bridge_node", anonymous=False)
-        rospy.Subscriber("/parrot/cmd_vel", Twist, self.cmd_vel_callback)
-
+        rospy.init_node('olympe_node', anonymous=False)
+        self.cmd_sub = rospy.Subscriber("/parrot/status_cmd", String, self.cmd_callback)
+        self.cmd_vel_sub = rospy.Subscriber("/parrot/cmd_vel", Twist, self.cmd_vel_callback)
+        self.isDroneConnected = False
         self.cmd_vel = Twist()
 
+        self.rate = rospy.Rate(10)
+        
         self.drone = olympe.Drone(DRONE_IP)
         self.drone.connect()
 
-        self.rate = rospy.Rate(10)
+        if self.drone.connection_state():
+            self.isDroneConnected = True
+            logger.success("Drone is connected and ready to takeoff!")
+        else:
+            logger.error("Drone disconnected!")
+            self.isDroneConnected = False
+            exit(0)
 
-        assert self.drone(TakeOff()).wait().success()
-        takeoff_str = "Takeoff successful %s" % rospy.get_time()
-        rospy.loginfo(takeoff_str)
-    
+        
     def cmd_vel_callback(self, msg: Twist):
         self.cmd_vel.linear.x = msg.linear.x
         self.cmd_vel.linear.y = msg.linear.y
         self.cmd_vel.linear.z = msg.linear.z
 
-        self.cmd_vel.angular.z = msg.angular.z
-
+        self.cmd_vel.angular.x = msg.angular.x
 
         print("cmd_vel received")
-        print("cmd_vel.z: ", self.cmd_vel.linear.z)
+        self.run()
 
-    
+
+    def cmd_callback(self, msg: String):
+        logger.info(msg.data)
+
+        if msg.data == "takeoff" or msg.data == "Takeoff" and self.isDroneConnected:
+            assert self.drone(TakeOff()).wait().success()
+
+        if msg.data == "land" or msg.data == "Land" and self.isDroneConnected:
+            assert self.drone(Landing()).wait().success()
+
+
     def run(self):
         self.drone(PCMD(
             1,
-            int(self.cmd_vel.linear.x),
             int(self.cmd_vel.linear.y),
+            int(self.cmd_vel.linear.x),
             int(self.cmd_vel.angular.z),
             int(self.cmd_vel.linear.z),
-            timestampAndSeqNum = 0,
-        )
-        )
+            timestampAndSeqNum = 0
+        ))
 
-        print("cmd_vel sent as PCMD")
-        print("x: ", int(self.cmd_vel.linear.x),
-              "y: ", int(self.cmd_vel.linear.x),
-              "z: ", int(self.cmd_vel.linear.x),
-              "psi: ", int(self.cmd_vel.angular.x))
+        logger.info("PCMD \nx: {}\ny: {}\nz: {}\nyaw: {}".format(self.cmd_vel.linear.x,
+                                                                 self.cmd_vel.linear.y,
+                                                                 self.cmd_vel.linear.z,
+                                                                 self.cmd_vel.angular.z))
+
+    def __del__(self):
+        self.drone.disconnect()
+        self.isDroneConnected = False
 
 
-def main():
+def main(args):
     olympe_bridge = OlympeBridge()
+    rospy.spin()
 
-    while not rospy.is_shutdown():
-        olympe_bridge.run()
-        olympe_bridge.rate.sleep()
 
-    assert olympe_bridge.drone(Landing()).wait().success()
-    landing_str = "Landing successful %s" % rospy.get_time()
-    rospy.loginfo(landing_str)
-
-    print("Shutting down")
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main(sys.argv)
