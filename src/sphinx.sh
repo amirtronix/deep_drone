42075b6df04da24ba3f10f14d4c22c32c4d5df12
amirtronics <amir.h.ebrahimnezhad@gmail.com>
2024-10-04 15:45:32 -0700

    sphinx.sh: update

--- src/sphinx.sh
+++ src/sphinx.sh
@@ -4,6 +4,6 @@ sudo systemctl start firmwared.service
 
 gnome-terminal -- sphinx "/opt/parrot-sphinx/usr/share/sphinx/drones/anafi_ai.drone"::firmware="/home/amirtronics/Downloads/anafi2-pc.ext2.zip"
 
-gnome-terminal -- parrot-ue4-empty -ams-path="DefaultPath,Pickup:*" -ams-path="RunPath,Jasper"
+gnome-terminal -- parrot-ue4-empty
 # gnome-terminal -- parrot-ue4-carla -level=town_02
 # gnome-terminal -- parrot-ue4-empty
